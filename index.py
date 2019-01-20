3209c6b6d9123e9607c062a737766c1b78f15e09
pedro <pedroluismontesinosrivera@gmail.com>
2019-01-19 21:19:27 -0500

    working on modifying the  hosts file

---
+++ index.py
@@ -0,0 +1,13 @@
+import time
+from datetime import datetime
+
+links = [
+    'www.blogger.com',
+    'blogger.com'
+]
+start = 10
+end = 18
+
+while True:
+    print('working...')
+    time.sleep(1)
