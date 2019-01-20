d2d63c4d3b8747ee3d19d30d2311d0cb61ff9e4b
pedro <pedroluismontesinosrivera@gmail.com>
2019-01-19 22:08:42 -0500

    websites are being added to the hosts file once we start the app

--- index.py
+++ index.py
@@ -1,13 +1,28 @@
 import time
-from datetime import datetime
+from datetime import datetime as dt
+
+hosts_path_windows = r"C:\Windows\System32\drivers\etc\hosts"
+hosts_path_unix = "/etc/hosts"
+host_dir = "hosts"
 
 links = [
     'www.blogger.com',
     'blogger.com'
 ]
-start = 10
-end = 18
+redirect = "127.0.0.1"
+start_time = 10
+end_time = 23
 
 while True:
-    print('working...')
+    if dt(dt.now().year, dt.now().month, dt.now().day, start_time) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, end_time):
+        with open(host_dir, 'r+') as file:
+            content = file.read()
+            for link in links:
+                if link in content:
+                    pass
+                else:
+                    file.write(redirect + " " + link + "\n")
+        print('working...')
+    else:
+        print('app is resting')
     time.sleep(1)
